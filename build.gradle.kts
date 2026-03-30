f206f024c48aa0d1e237dddf25bf803ff9d7b5a8
HiorCraft <liam.fanenbruck@gmail.com>
2026-03-30 17:58:00 +0200

    Feat: add LuckPerms Hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,16 @@ plugins {
 group = "de.hiorcraft.nex.nexchat"
 version = findProperty("version") as String
 
+repositories {
+    mavenCentral()
+    maven("https://repo.lucko.me/")
+}
+
+dependencies {
+    compileOnly("net.luckperms:api:5.4")
+}
+
 surfPaperPluginApi {
     mainClass("de.hiorcraft.nex.nexchat.BukkitMain")
     authors.add("HiorCraft")
-
-    runServer {
-        withSurfApiBukkit()
-    }
 }
