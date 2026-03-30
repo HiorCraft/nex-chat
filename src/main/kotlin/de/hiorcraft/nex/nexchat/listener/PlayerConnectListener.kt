2284715a7b6445710ec04680f98b3a86228fbd4c
HiorCraft <liam.fanenbruck@gmail.com>
2026-03-30 17:47:57 +0200

    Feat: Re-name Class

--- src/main/kotlin/de/hiorcraft/nex/nexchat/listener/PlayerConnectListener.kt
+++ src/main/kotlin/de/hiorcraft/nex/nexchat/listener/PlayerConnectListener.kt
@@ -9,7 +9,7 @@ import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 
-class PlayerConnect : Listener {
+class PlayerConnectListener : Listener {
 
     @EventHandler
 
